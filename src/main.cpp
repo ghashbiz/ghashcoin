3ade86cfcbf11846b024e7ef202ad603daf1a20b
root <root@ghash.biz>
2017-11-22 11:14:26 +0200

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("08613bd67d9edd941b6d451cbb13fb5ff595ed7ab1d3525798653adfe3dd2b62");
+uint256 hashGenesisBlock("888c2710cf561f81c05ef095a428fa35841f1aeed79e966e9c5269d887967804");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -2031,9 +2031,9 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1511339359; //epochtime
+        block.nTime    = 1511341505; //epochtime
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 389867836;
+        block.nNonce   = 390348231;
 
         if (fTestNet)
         {
@@ -2048,7 +2048,7 @@ bool LoadBlockIndex(bool fAllowNew)
         assert(block.hashMerkleRoot == uint256("5a2e19825b4162f68602039040f1e05d9f924ff00a3aff7327ca6abd6f3279bc"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (true && block.GetHash() != hashGenesisBlock)
+        if (false && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
